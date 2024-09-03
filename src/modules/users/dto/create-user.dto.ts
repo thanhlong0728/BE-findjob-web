c2ff013bb03dd91a4bdc425c02550f2f1d39cc35
LongThV <longvt@flydinotech.com>
2024-09-04 00:24:55 +0700

    config debug

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  name: string;
+
+  email: string;
+
+  password: string;
+
+  phone: string;
+
+  address: string;
+
+  image: string;
+}

18b0ca623e222840879dbbbd18d91973720f5e09
thanhlong0728 <thanhlong0728@gmail.com>
2024-10-16 14:45:05 +0700

    login jwt

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -8,3 +8,14 @@ export const hashPasswordHelper = async (plainPassword: string) => {
     console.log(err);
   }
 };
+
+export const comparePasswordHelper = async (
+  plainPassword: string,
+  hashPassword: string,
+) => {
+  try {
+    return await bcrypt.compare(plainPassword, hashPassword);
+  } catch (err) {
+    console.log(err);
+  }
+};

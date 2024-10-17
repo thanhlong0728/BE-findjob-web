bb6e06e0f994d3ac0e4f3cf1c202b4c5e6babc94
thanhlong0728 <thanhlong0728@gmail.com>
2024-10-17 09:24:11 +0700

    jwt global guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,18 +12,19 @@ import {
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { JwtAuthGuard } from './passport/jwt-auth.guard';
+import { Public } from '@/decorator/customize';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  @UseGuards(LocalAuthGuard)
+  @Public()
   handleLogin(@Request() req) {
     return this.authService.login(req.user);
   }
 
-  @UseGuards(JwtAuthGuard)
+  // @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;

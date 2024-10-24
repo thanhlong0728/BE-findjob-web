1642e35e8daa37b07c109d56bc32c6e63fc432bc
thanhlong0728 <thanhlong0728@gmail.com>
2024-10-24 16:39:22 +0700

    resend email

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -114,7 +114,6 @@ export class UsersService {
     this.mailerService.sendMail({
       to: email, // list of receivers
       subject: 'Activate your account at @longv', // Subject line
-      text: 'welcome', // plaintext body
       template: 'register',
       context: {
         name: user?.name ?? user?.email,
@@ -150,5 +149,34 @@ export class UsersService {
     } else {
       throw new BadRequestException('Mã code không hợp lệ hoặc hết hạn');
     }
+  }
+
+  async handleRetryActive(email: string) {
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new BadRequestException('Tài khoản không tồn tại');
+    }
+    if (user.isActive) {
+      throw new BadRequestException('Tài khoản đã được kích hoạt');
+    }
+
+    //update user
+    const codeId = uuidv4();
+    await user.updateOne({
+      codeId: codeId,
+      codeExpired: dayjs().add(5, 'minutes'),
+    });
+
+    // send email
+    this.mailerService.sendMail({
+      to: email, // list of receivers
+      subject: 'Activate your account at @longv', // Subject line
+      template: 'register',
+      context: {
+        name: user?.name ?? user?.email,
+        activationCode: codeId,
+      },
+    });
+    return { _id: user._id };
   }
 }
